17bd0cb70109d4f4538bef952996872dee1fdd8a
AliiAhmadi <aliahmadi.github@gmail.com>
2023-11-28 22:25:40 +0330

    first controller

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", home)
+
+	log.Println("Listen on localhost:4000")
+	err := http.ListenAndServe(":4000", mux)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func home(writer http.ResponseWriter, request *http.Request) {
+	writer.Write([]byte("Home page"))
 }

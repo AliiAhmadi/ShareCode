cb72d2b5ccc9c2cafdc524c9f3c2c51923695fca
AliiAhmadi <aliahmadi.github@gmail.com>
2023-11-29 15:40:27 +0330

    config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,21 +4,27 @@ import (
 	"flag"
 	"log"
 	"net/http"
+
+	config "github.com/AliiAhmadi/ShareCode/config"
 )
 
 func main() {
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	address := flag.String("addr", ":4000", "HTTP network address")
+	config := new(config.Config)
+
+	flag.IntVar(&config.Port, "port", 4000, "Listen port")
+	flag.StringVar(&config.Address, "addr", "127.0.0.1", "HTTP network address")
+
 	flag.Parse()
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on " + *address)
-	err := http.ListenAndServe(*address, mux)
+	log.Println("Starting server on " + config.Get())
+	err := http.ListenAndServe(config.Get(), mux)
 	if err != nil {
 		log.Fatal(err)
 	}
---
+++ config/config.go
@@ -0,0 +1,12 @@
+package config
+
+import "strconv"
+
+type Config struct {
+	Port    int
+	Address string
+}
+
+func (config *Config) Get() string {
+	return config.Address + ":" + strconv.Itoa(config.Port)
+}

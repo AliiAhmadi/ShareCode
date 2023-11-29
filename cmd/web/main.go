648f6888ee5623f469f6bb08f9b095ae5ece8e73
AliiAhmadi <aliahmadi.github@gmail.com>
2023-11-29 11:00:13 +0330

    add links to base template

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,11 +6,13 @@ import (
 )
 
 func main() {
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)

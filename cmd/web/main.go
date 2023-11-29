5121e8faec415c29829fdda24f92be7adf57b10e
AliiAhmadi <aliahmadi.github@gmail.com>
2023-11-29 15:17:42 +0330

    get HTTP address with flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	address := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
@@ -14,8 +17,8 @@ func main() {
 	mux.HandleFunc("/snippet/create", createSnippet)
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Println("Starting server on " + *address)
+	err := http.ListenAndServe(*address, mux)
 	if err != nil {
 		log.Fatal(err)
 	}

eff369550e241c8f98f26369bdc7b2922ca597d0
AliiAhmadi <aliahmadi.github@gmail.com>
2023-11-29 08:16:57 +0330

    change structure of project

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,25 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
-
-func main() {
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-
-	log.Println("Listen on localhost:4000")
-	err := http.ListenAndServe(":4000", mux)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-}
 
 func home(writer http.ResponseWriter, request *http.Request) {
 
@@ -29,11 +13,10 @@ func home(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	writer.Write([]byte("Welcome to ShareCode"))
+	writer.Write([]byte("Hello from ShareCode"))
 }
 
 func showSnippet(writer http.ResponseWriter, request *http.Request) {
-
 	id, err := strconv.Atoi(request.URL.Query().Get("id"))
 
 	if err != nil || id < 1 {
@@ -41,18 +24,16 @@ func showSnippet(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(writer, "Snippet with id %d", id)
+	fmt.Fprintf(writer, "Display snippet with ID %d ...", id)
 }
 
 func createSnippet(writer http.ResponseWriter, request *http.Request) {
 
 	if request.Method != "POST" {
 		writer.Header().Set("Allow", "POST")
-		// writer.WriteHeader(405)
-		// writer.Write([]byte("Method not allowed"))
 		http.Error(writer, "Method Not Allowed", 405)
 		return
 	}
 
-	writer.Write([]byte("Create a new snippet..."))
+	writer.Write([]byte("Creating a new snippet..."))
 }

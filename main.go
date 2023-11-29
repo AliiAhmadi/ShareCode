2a8376fa465e79190bf906a039a1279faebcc096
AliiAhmadi <aliahmadi.github@gmail.com>
2023-11-29 06:43:53 +0330

    add more routes

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Listen on localhost:4000")
 	err := http.ListenAndServe(":4000", mux)
@@ -19,5 +21,13 @@ func main() {
 }
 
 func home(writer http.ResponseWriter, request *http.Request) {
-	writer.Write([]byte("Home page"))
+	writer.Write([]byte("Welcome to ShareCode"))
+}
+
+func showSnippet(writer http.ResponseWriter, request *http.Request) {
+	writer.Write([]byte("Display a specific snippet..."))
+}
+
+func createSnippet(writer http.ResponseWriter, request *http.Request) {
+	writer.Write([]byte("Create a new snippet..."))
 }

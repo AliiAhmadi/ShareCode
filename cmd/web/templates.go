53d571189c80143fe6152314f8e0a9e128e16b95
AliiAhmadi <aliahmadi.github@gmail.com>
2023-12-01 21:03:30 +0330

    add in memory cache for store template files

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,58 @@
 package main
 
-import "github.com/AliiAhmadi/ShareCode/pkg/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"github.com/AliiAhmadi/ShareCode/pkg/models"
+)
 
 type templateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache(dir string) (map[string]*template.Template, error) {
+	// Initialize a new map to act as the cache.
+	cache := map[string]*template.Template{}
+
+	// Use filepath.Glob function to get a slice of all filepaths with the
+	// extension '.page.tmpl'. This essentially gives us a slice of all the
+	// 'page' templates for the application.
+	pages, err := filepath.Glob(filepath.Join(dir, "*.page.tmpl"))
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Loop through the pages.
+	for _, page := range pages {
+		// Extract the file name.
+		name := filepath.Base(page)
+
+		// Parse the page template file.
+		ts, err := template.ParseFiles(page)
+
+		if err != nil {
+			return nil, err
+		}
+
+		// Use the ParseGlob method to add any 'layout' templates.
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.layout.tmpl"))
+		if err != nil {
+			return nil, err
+		}
+
+		// Use the ParseGlob method to add any 'partial' templates.
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.partial.tmpl"))
+		if err != nil {
+			return nil, err
+		}
+
+		// Add the template set to the cache.
+		cache[name] = ts
+	}
+
+	// Return the map :)
+	return cache, nil
 }

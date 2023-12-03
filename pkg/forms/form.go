ed064ef169acdfe81614c5a3c9d49729fc9fde0b
AliiAhmadi <aliahmadi.github@gmail.com>
2023-12-03 20:39:32 +0330

    centralizing form data validation in form package

--- pkg/forms/form.go
+++ pkg/forms/form.go
@@ -1,8 +1,40 @@
 package forms
 
-import "net/url"
+import (
+	"fmt"
+	"net/url"
+	"strings"
+	"unicode/utf8"
+)
 
 type Form struct {
 	url.Values
 	Errors errors
 }
+
+func New(data url.Values) *Form {
+	return &Form{
+		data,
+		errors(map[string][]string{}),
+	}
+}
+
+func (form *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := form.Get(field)
+
+		if strings.TrimSpace(value) == "" {
+			form.Errors.Add(field, "This field can not be blank.")
+		}
+	}
+}
+
+func (form *Form) MaxLength(field string, length int) {
+	value := form.Get(field)
+
+	if utf8.RuneCountInString(value) > length {
+		form.Errors.Add(field, fmt.Sprintf("This field is too long. (Max length = %d)", length))
+	}
+}
+
+// func (form *Form)

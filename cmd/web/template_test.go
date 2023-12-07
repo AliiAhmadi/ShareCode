8109a8a910d3a8a3320d2122dabf613bef13c3de
AliiAhmadi <aliahmadi.github@gmail.com>
2023-12-07 09:19:34 +0330

    template testing - humanDate - update

--- cmd/web/template_test.go
+++ cmd/web/template_test.go
@@ -6,10 +6,36 @@ import (
 )
 
 func TestHumanDate(t *testing.T) {
-	tm := time.Date(2023, 12, 3, 5, 0, 0, 0, time.UTC)
-	hd := humanDate(tm)
 
-	if hd != "03 Dec 2023 at 05:00" {
-		t.Errorf("walt: %s. got: %s", "3 Dec 2023 at 5:00", hd)
+	tests := []struct {
+		name string
+		tm   time.Time
+		want string
+	}{
+		{
+			name: "UTC",
+			tm:   time.Date(2020, 12, 17, 10, 0, 0, 0, time.UTC),
+			want: "17 Dec 2020 at 10:00",
+		},
+		{
+			name: "Empty",
+			tm:   time.Time{},
+			want: "",
+		},
+		{
+			name: "CET",
+			tm:   time.Date(2020, 12, 17, 9, 0, 0, 0, time.FixedZone("CET", 1*6)),
+			want: "17 Dec 2020 at 09:00",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			hd := humanDate(test.tm)
+
+			if hd != test.want {
+				t.Errorf("want: %s. got: %s", test.want, hd)
+			}
+		})
 	}
 }
